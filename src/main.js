1ac8850e3fa97318de13ad23ce2c10e0f5e26300
satawatnack <60070093@kmitl.ac.th>
2020-03-08 15:06:42 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
+import firebase from 'firebase'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCNtXlRX0F3WRdtqTHqkjScvO9Tu-M0d1Q",
+  authDomain: "progressiveproj.firebaseapp.com",
+  databaseURL: "https://progressiveproj.firebaseio.com",
+  projectId: "progressiveproj",
+  storageBucket: "progressiveproj.appspot.com",
+  messagingSenderId: "741379373687",
+  appId: "1:741379373687:web:26d888271dec148f74e472",
+  measurementId: "G-F6L64L2FT7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
---
+++ src/views/SignUp.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="sign-up">
+        <h3>Let's create a new account !</h3>
+        <input type="text" placeholder="Email" name="" id=""><br>
+        <input type="password" placeholder="Password" name="" id=""><br>
+        <button>Sign up</button>
+        <span>or go back to <router-link to="login">login.</router-link></span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'signUp',
+    data() {
+        return {}
+    },
+    methods: {}
+}
+</script>
+
+<style scoped>
+    .login {
+        margin-top: 40px;
+    }
+    input {
+        margin: 10px 0;
+        width: 20%;
+        padding: 15px;
+    }
+    button {
+        margin-top: 20px;
+        width: 10%;
+        cursor: pointer;
+    }
+    span {
+        display: block;
+        margin-top: 10px;
+        font-size: 11px;
+    }
+</style>

addd653f63964fc68453f2b3230f5e72ccc1cc27
satawatnack <60070093@kmitl.ac.th>
2020-05-24 10:56:37 +0700

    hide firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,6 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCNtXlRX0F3WRdtqTHqkjScvO9Tu-M0d1Q",
-    authDomain: "progressiveproj.firebaseapp.com",
-    databaseURL: "https://progressiveproj.firebaseio.com",
-    projectId: "progressiveproj",
-    storageBucket: "progressiveproj.appspot.com",
-    messagingSenderId: "741379373687",
-    appId: "1:741379373687:web:26d888271dec148f74e472",
-    measurementId: "G-F6L64L2FT7"
 }
 
 firebase.initializeApp(firebaseConfig)
